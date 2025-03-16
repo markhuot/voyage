074cfc13a0cc57b60a908db1b65a2140b29ac34f
Mark Huot <mark@markhuot.com>
2025-03-16 05:32:12 -0400

    Update namespace from etl to voyageupdating namespace

--- src/auditors/Sqlite.php
+++ src/auditors/Sqlite.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace markhuot\etl\auditors;
+namespace markhuot\voyage\auditors;
 
 use DateTime;
-use markhuot\etl\base\AuditorInterface;
-use markhuot\etl\base\Frame;
+use markhuot\voyage\base\AuditorInterface;
+use markhuot\voyage\base\Frame;
 use PDO;
 use stdClass;
 use Throwable;
